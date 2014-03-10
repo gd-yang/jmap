84012d666c45d7e1fd3c7299401cbf186853d1f7
yangfei <86787605@qq.com>
2014-03-10 20:34:13 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    Conflicts:
    	public/mapeditor/src/mode/DrawPolygon.js

---
+++ public/sorting/lib/paras.js
@@ -0,0 +1,19 @@
+define(function (require, exports, module){
+    function getURLParas(){
+        var url = window.location.href, obj={};
+        url = url.split('?');
+        url = !!url && url.length > 0 ? url[1] : null;
+        url = !!url ? url.split('&') : url;
+        if (!!url){
+            url.forEach(function(para){
+                para = para.split('=');
+                if (!!para){
+                    obj[para[0]] = para[1];
+                }
+            });
+        }
+        url = obj;
+        return url;
+    }
+    return getURLParas;
+});
