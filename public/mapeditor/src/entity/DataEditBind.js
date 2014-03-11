7ef26f6e1db209dd5889f8ccebac7fae6b254ba9
yangfei <86787605@qq.com>
2014-03-11 10:05:58 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    Conflicts:
    	public/mapeditor/src/mode/DrawPolygon.js

--- public/mapeditor/src/entity/DataEditBind.js
+++ public/mapeditor/src/entity/DataEditBind.js
@@ -14,18 +14,17 @@ ME.Entity.DataEditBind = L.extend({}, ME.Entity.EditBind, {
             return;
         }
         this.edited = true;
-        this.on('click', function () {
-            console.log(111)
-        });
+
         if (this.editing) {
             this.editing.enable();
         }
-        this.dragging.enable();
+
         this.on('selectIn', this._fireSelect, this)
             .on('mouseover', this._fireOver, this)
             .on('mouseout', this._fireOut, this)
             .on('selectOut', this._fireSelectOut, this)
             .on('edit', this._fireChanges, this);
+        this.dragging.enable();
         // 暂时，以后转移到菜单触发
         if (this.dragging.on) {
             this.dragging.on('dragend', this._fireDragEnd, this);
@@ -41,12 +40,13 @@ ME.Entity.DataEditBind = L.extend({}, ME.Entity.EditBind, {
         if (this.editing) {
             this.editing.disable();
         }
-        this.dragging.disable();
+
         this.off('selectIn', this._fireSelect, this)
             .off('mouseover', this._fireOver, this)
             .off('mouseout', this._fireOut, this)
             .off('selectOut', this._fireSelectOut, this)
             .off('edit', this._fireChanges, this);
+        this.dragging.disable();
         // 暂时，以后转移到菜单触发
         if (this.dragging.off) {
             this.dragging.off('dragend', this._fireDragEnd, this);
